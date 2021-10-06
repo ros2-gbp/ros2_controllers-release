2efd8d1d76af6bb2aa6e960a9864da40f0ae0414
Bence Magyar <bence.magyar.robotics@gmail.com>
2021-10-06 11:43:07 +0100

    Rebase from 'upstream'

--- diff_drive_controller/include/diff_drive_controller/diff_drive_controller.hpp
+++ diff_drive_controller/include/diff_drive_controller/diff_drive_controller.hpp
@@ -55,16 +55,17 @@ public:
   DiffDriveController();
 
   DIFF_DRIVE_CONTROLLER_PUBLIC
-  controller_interface::return_type init(const std::string & controller_name) override;
-
-  DIFF_DRIVE_CONTROLLER_PUBLIC
   controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
   DIFF_DRIVE_CONTROLLER_PUBLIC
   controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
   DIFF_DRIVE_CONTROLLER_PUBLIC
-  controller_interface::return_type update() override;
+  controller_interface::return_type update(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  DIFF_DRIVE_CONTROLLER_PUBLIC
+  CallbackReturn on_init() override;
 
   DIFF_DRIVE_CONTROLLER_PUBLIC
   CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
