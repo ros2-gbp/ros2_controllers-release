6c89eeb0a1fc69231bfa76fd17812342a60134d8
Bence Magyar <bence.magyar.robotics@gmail.com>
2024-07-24 11:39:56 +0100

    Imported upstream version '3.26.0' of 'upstream'

--- pid_controller/src/pid_controller.cpp
+++ pid_controller/src/pid_controller.cpp
@@ -32,16 +32,10 @@ namespace
 {  // utility
 
 // Changed services history QoS to keep all so we don't lose any client service calls
-static const rmw_qos_profile_t qos_services = {
-  RMW_QOS_POLICY_HISTORY_KEEP_ALL,
-  1,  // message queue depth
-  RMW_QOS_POLICY_RELIABILITY_RELIABLE,
-  RMW_QOS_POLICY_DURABILITY_VOLATILE,
-  RMW_QOS_DEADLINE_DEFAULT,
-  RMW_QOS_LIFESPAN_DEFAULT,
-  RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT,
-  RMW_QOS_LIVELINESS_LEASE_DURATION_DEFAULT,
-  false};
+rclcpp::QoS qos_services =
+  rclcpp::QoS(rclcpp::QoSInitialization(RMW_QOS_POLICY_HISTORY_KEEP_ALL, 1))
+    .reliable()
+    .durability_volatile();
 
 using ControllerCommandMsg = pid_controller::PidController::ControllerReferenceMsg;
 
@@ -385,7 +379,8 @@ controller_interface::CallbackReturn PidController::on_deactivate(
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
-controller_interface::return_type PidController::update_reference_from_subscribers()
+controller_interface::return_type PidController::update_reference_from_subscribers(
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
   auto current_ref = input_ref_.readFromRT();
 
