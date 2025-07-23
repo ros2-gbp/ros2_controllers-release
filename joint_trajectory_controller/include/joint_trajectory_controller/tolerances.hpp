c3a8efcc6a7c72dfaa3b8b58a6380f5db96e07fa
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-07-23 06:37:01 +0000

    Imported upstream version '5.4.0' of 'upstream'

--- joint_trajectory_controller/include/joint_trajectory_controller/tolerances.hpp
+++ joint_trajectory_controller/include/joint_trajectory_controller/tolerances.hpp
@@ -38,9 +38,6 @@
 #include "control_msgs/action/follow_joint_trajectory.hpp"
 #include "joint_trajectory_controller/joint_trajectory_controller_parameters.hpp"
 
-#include "rclcpp/node.hpp"
-#include "rclcpp/time.hpp"
-
 namespace joint_trajectory_controller
 {
 /**
@@ -319,7 +316,7 @@ inline bool check_state_tolerance_per_joint(
   if (show_errors)
   {
     const auto logger = rclcpp::get_logger("tolerances");
-    RCLCPP_ERROR(logger, "State tolerances failed for joint %d:", joint_idx);
+    RCLCPP_ERROR(logger, "State tolerances failed for joint %lu:", joint_idx);
 
     if (state_tolerance.position > 0.0 && abs(error_position) > state_tolerance.position)
     {
