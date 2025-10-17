f59dd6fa1c4c6769879f8c6c5881e15894a7e06a
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-10-17 08:14:10 +0000

    Imported upstream version '4.33.1' of 'upstream'

--- force_torque_sensor_broadcaster/test/test_force_torque_sensor_broadcaster.hpp
+++ force_torque_sensor_broadcaster/test/test_force_torque_sensor_broadcaster.hpp
@@ -19,10 +19,10 @@
 #ifndef TEST_FORCE_TORQUE_SENSOR_BROADCASTER_HPP_
 #define TEST_FORCE_TORQUE_SENSOR_BROADCASTER_HPP_
 
+#include <gmock/gmock.h>
+
 #include <memory>
 #include <string>
-
-#include "gmock/gmock.h"
 
 #include "force_torque_sensor_broadcaster/force_torque_sensor_broadcaster.hpp"
 
@@ -39,6 +39,11 @@ class FriendForceTorqueSensorBroadcaster
   FRIEND_TEST(ForceTorqueSensorBroadcasterTest, SensorName_ActivateDeactivate_Success);
   FRIEND_TEST(ForceTorqueSensorBroadcasterTest, UpdateTest);
   FRIEND_TEST(ForceTorqueSensorBroadcasterTest, SensorStatePublishTest);
+
+  FRIEND_TEST(ForceTorqueSensorBroadcasterTest, SensorFilterChain_Configure_Success);
+  FRIEND_TEST(ForceTorqueSensorBroadcasterTest, SensorFilterChain_ActivateDeactivate_Success);
+  FRIEND_TEST(ForceTorqueSensorBroadcasterTest, SensorFilterChain_Update_Success);
+  FRIEND_TEST(ForceTorqueSensorBroadcasterTest, SensorFilterChain_Publish_Success);
 };
 
 class ForceTorqueSensorBroadcasterTest : public ::testing::Test
@@ -50,12 +55,12 @@ public:
   void SetUp();
   void TearDown();
 
-  void SetUpFTSBroadcaster();
+  void SetUpFTSBroadcaster(std::string node_name);
 
 protected:
   const std::string sensor_name_ = "fts_sensor";
   const std::string frame_id_ = "fts_sensor_frame";
-  std::array<double, 6> sensor_values_ = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
+  std::array<double, 6> sensor_values_ = {{1.1, 2.2, 3.3, 4.4, 5.5, 6.6}};
 
   hardware_interface::StateInterface fts_force_x_{sensor_name_, "force.x", &sensor_values_[0]};
   hardware_interface::StateInterface fts_force_y_{sensor_name_, "force.y", &sensor_values_[1]};
@@ -66,7 +71,8 @@ protected:
 
   std::unique_ptr<FriendForceTorqueSensorBroadcaster> fts_broadcaster_;
 
-  void subscribe_and_get_message(geometry_msgs::msg::WrenchStamped & wrench_msg);
+  void subscribe_and_get_message(
+    geometry_msgs::msg::WrenchStamped & wrench_msg, std::string & topic_name);
 };
 
 #endif  // TEST_FORCE_TORQUE_SENSOR_BROADCASTER_HPP_
