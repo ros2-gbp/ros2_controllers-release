ac37bf86815f5ce8881b704f97f0afcb9101700e
Bence Magyar <bence.magyar.robotics@gmail.com>
2026-08-10 23:04:05 +0100

    Rebase from 'upstream'

--- mecanum_drive_controller/test/test_mecanum_drive_controller.hpp
+++ mecanum_drive_controller/test/test_mecanum_drive_controller.hpp
@@ -107,7 +107,9 @@ public:
   controller_interface::CallbackReturn on_activate(
     const rclcpp_lifecycle::State & previous_state) override
   {
-    auto ref_itfs = on_export_reference_interfaces();
+    // export_reference_interfaces() populates ordered_exported_reference_interfaces_
+    export_reference_interfaces();
+    export_state_interfaces();
     return mecanum_drive_controller::MecanumDriveController::on_activate(previous_state);
   }
 
@@ -193,10 +195,11 @@ protected:
 
     for (size_t i = 0; i < joint_command_values_.size(); ++i)
     {
-      command_itfs_.emplace_back(
-        hardware_interface::CommandInterface(
-          command_joint_names_[i], interface_name_, &joint_command_values_[i]));
-      command_ifs.emplace_back(command_itfs_.back());
+      auto cmd_itf = std::make_shared<hardware_interface::CommandInterface>(
+        command_joint_names_[i], interface_name_);
+      std::ignore = cmd_itf->set_value(joint_command_values_[i]);
+      command_itfs_.emplace_back(cmd_itf);
+      command_ifs.emplace_back(command_itfs_.back(), nullptr);
     }
 
     std::vector<hardware_interface::LoanedStateInterface> state_ifs;
@@ -205,10 +208,11 @@ protected:
 
     for (size_t i = 0; i < joint_state_values_.size(); ++i)
     {
-      state_itfs_.emplace_back(
-        hardware_interface::StateInterface(
-          command_joint_names_[i], interface_name_, &joint_state_values_[i]));
-      state_ifs.emplace_back(state_itfs_.back());
+      auto state_itf = std::make_shared<hardware_interface::StateInterface>(
+        command_joint_names_[i], interface_name_);
+      std::ignore = state_itf->set_value(joint_state_values_[i]);
+      state_itfs_.emplace_back(state_itf);
+      state_ifs.emplace_back(state_itfs_.back(), nullptr);
     }
 
     controller_->assign_interfaces(std::move(command_ifs), std::move(state_ifs));
@@ -319,8 +323,8 @@ protected:
   static constexpr double TEST_ANGULAR_VELOCITY_Z = 0.0;
   double command_lin_x = 111;
 
-  std::vector<hardware_interface::StateInterface> state_itfs_;
-  std::vector<hardware_interface::CommandInterface> command_itfs_;
+  std::vector<hardware_interface::StateInterface::SharedPtr> state_itfs_;
+  std::vector<hardware_interface::CommandInterface::SharedPtr> command_itfs_;
 
   double ref_timeout_ = 0.1;
 
